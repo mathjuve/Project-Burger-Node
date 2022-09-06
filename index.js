4c8cc290599729101b68369521a745b0a1b5dbfc
mathjuve <matheus.98562020@gmail.com>
2022-09-05 21:24:57 -0300

    My first project Node

---
+++ index.js
@@ -0,0 +1,104 @@
+const { request } = require("express");
+const express = require("express");
+const uuid = require("uuid");
+const cors = require("cors");
+
+const app = express();
+const port = 3001;
+app.use(express.json());
+app.use(cors());
+
+// Armazenar os pedidos
+const orders = [];
+
+// Middleware validação de ID
+const checkUserId = (request, response, next) => {
+  const { id } = request.params;
+
+  const index = orders.findIndex((order) => order.id === id);
+
+  if (index < 0) {
+    return response.status(404).json({ error: "Order not found" });
+  }
+
+  request.userIndex = index;
+  request.userId = id;
+
+  next();
+};
+
+// Lista de todos os pedidos
+app.get("/orders", (request, response) => {
+  return response.json(orders);
+});
+
+// Criação de pedido
+app.post("/orders", (request, response) => {
+  const { name, order, price } = request.body;
+
+  const client = {
+    id: uuid.v4(),
+    name,
+    order,
+    price,
+    status: "In preparation",
+  };
+
+  orders.push(client);
+
+  return response.status(201).json(client);
+});
+
+// Atualização de pedido
+app.put("/orders/:id", checkUserId, (request, response) => {
+  const { name, order, price } = request.body;
+  const id = request.userId;
+  const index = request.userIndex;
+
+  const updateOrder = { id, name, order, price, status: "In preparation" };
+
+  orders[index] = updateOrder;
+
+  return response.json(updateOrder);
+});
+
+// Deletar um pedido
+app.delete("/orders/:id", checkUserId, (request, response) => {
+  const index = request.userIndex;
+
+  orders.splice(index, 1);
+
+  return response.status(204).json();
+});
+
+// Consultar um pedido específico
+app.get("/orders/:id", checkUserId, (request, response) => {
+  const id = request.userId;
+  const index = request.userIndex;
+
+  const { name, order, price, status } = orders[index];
+
+  const queryOrder = { id, name, order, price, status };
+
+  orders[index] = queryOrder;
+
+  return response.json(queryOrder);
+});
+
+// Alteração de status para "Pronto".
+app.patch("/orders/:id", checkUserId, (request, response) => {
+  const id = request.userId;
+  const index = request.userIndex;
+
+  const { name, order, price } = orders[index];
+
+  const queryOrder = { id, name, order, price, status: "Ready order!" };
+
+  orders[index] = queryOrder;
+
+  return response.json(queryOrder);
+});
+
+app.listen(port, () => {
+  console.log(`✅ Server starded on port ${port}`);
+});
